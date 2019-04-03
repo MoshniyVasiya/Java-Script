bf65a2c3d68b73a9266a8abad5aac9ed08d99ac3
MoshniyVasiya <vasyashapran93@gmail.com>
2019-04-03 14:27:20 +0300

    Hometask 17

---
+++ script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// 1
+
+var counter = ( 3 * 60) * 60;
+console.log(counter);
+
+
+// 2
+
+var num = 1;
+num+= 12;
+num-= 14;
+num*= 5;
+num/= 7;
+num++;
+num--;
+alert(num);
+
+
+// 3
+
+var a = 10, b = 2, c = 5;
+console.log( a + b + c);
+
+
+// 4
+
+var a = 17, b = 10, c = a - b, d = 7;
+var result = c + d;
+console.log(result);
+
+
+// 5
+
+var name = 'Вася!';
+alert('Привет, ' + name );
